080078913706b971c5d4c15a96c678072bf89f93
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-14 12:28:26 +0200

    Remove material dependency from alerting

--- src/lib/alerting/alert.service.ts
+++ src/lib/alerting/alert.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
 import {AlertComponent} from "./alert/alert.component";
+import {Overlay} from "@angular/cdk/overlay";
+import { ComponentPortal } from '@angular/cdk/portal';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,26 @@ export class AlertService {
 
   static instance: AlertService;
   constructor(
-    private dialog: MatDialog,
+      private readonly overlay: Overlay
   ) {
     AlertService.instance = this;
   }
 
+  alert(msg: string): void {
+    const overlayRef = this.overlay.create({
+      width: '20%',
+      height: '30%',
+      hasBackdrop: true,
+      backdropClass: 'cdk-overlay-dark-backdrop',
+      panelClass: 'alert-overlay'
+    });
+    const componentRef = overlayRef.attach(
+        new ComponentPortal(AlertComponent)
+    );
+    componentRef.instance.message = msg;
 
-  alert(msg: string) {
-    const dialogRef = this.dialog.open(
-      AlertComponent,
-      {width: '20%', height: '30%'}
-    )
-    dialogRef.componentInstance.message = msg;
+    overlayRef.backdropClick().subscribe(() => {
+      overlayRef.dispose();
+    });
   }
 }
