55bef19a133084fbce56a34e3e92f11432b9f8f6
Susanne Göbel <goebel@uni-koblenz.de>
2026-09-15 09:02:07 +0200

    Add tests for input handler

--- src/lib/io/input-handler.spec.ts
+++ src/lib/io/input-handler.spec.ts
@@ -5,4 +5,22 @@ describe('InputHandler', () => {
   it('should create an instance', () => {
     expect(new InputHandler()).toBeTruthy();
   });
+
+  it('gets a number from an input event', () => {
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.value = '42';
+
+    const event = { target: input } as unknown as Event;
+    expect(InputHandler.getNewValueFromEvent(event)).toBe(42);
+  });
+
+  it('clears the input element', () => {
+    const input = document.createElement('input');
+    input.value = '42';
+    const event = { target: input } as unknown as Event;
+
+    InputHandler.clearElem(event);
+    expect(input.value).toBe('');
+  });
 });
